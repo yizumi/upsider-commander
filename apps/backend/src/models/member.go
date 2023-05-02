61183f6ee694b7be29514912022f0678f6b83008
yizumi@raksul.com <yizumi@mbp1.local>
2023-05-02 11:51:59 +0900

    First commit

--- apps/backend/src/models/member.go
+++ apps/backend/src/models/member.go
@@ -1,15 +1,17 @@
 package models
 
 import (
-	"gorm.io/gorm"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type Member struct {
-	ID        uint           `gorm:"primaryKey" json:"id"`
-	Name      string         `gorm:"not null" json:"name"`
-	Email     string         `gorm:"unique;not null" json:"email"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	ID             uint           `gorm:"primaryKey" json:"id"`
+	OrganizationID uint           `gorm:"not null" json:"organization_id"`
+	Name           string         `gorm:"not null" json:"name"`
+	Email          string         `gorm:"unique;not null" json:"email"`
+	CreatedAt      time.Time      `json:"created_at"`
+	UpdatedAt      time.Time      `json:"updated_at"`
+	DeletedAt      gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
